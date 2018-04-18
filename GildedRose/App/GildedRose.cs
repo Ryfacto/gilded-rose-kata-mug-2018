a7cd448af1026c7138988d44454f12e3263d586b
Nicolas VERINAUD <nicolas.verinaud@ryfacto.fr>
2018-04-18 08:39:12 +0200

    100% coverage but only testing 4 days : 0, 1, 2, 1000.

--- GildedRose/App/GildedRose.cs
+++ GildedRose/App/GildedRose.cs
@@ -4,7 +4,7 @@ namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
+        public IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -84,6 +84,37 @@ namespace csharp
                     }
                 }
             }
+        }
+
+        public static List<Item> CreateDefaultItems()
+        {
+            return new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
         }
     }
 }
